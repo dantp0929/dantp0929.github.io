7584c3f366aaee5cdd09baca71463950290939a1
Daniel Phan <phand972@gmail.com>
2022-05-24 16:16:22 -0500

    First Intern Snippet Created

--- my-website/src/components/snippet.js
+++ my-website/src/components/snippet.js
@@ -1,12 +1,55 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
+/**
+ * Props used:
+ * imageSource - Path to image, passed with required("/path/to/image")
+ * imageAlt - Alt for image
+ * details - Usually text about the project / experience
+ * logos - An array of logos of the involved tools used
+ *    - source: Passed with required("/path/to/image")
+ *    - alt: Alt of image
+ * buttons - An array of buttons for links and whatnot
+ *    - link: http string to website
+ *    - text: text on the button
+ *    - variant: variant of the button, by Bootstrap
+ */
 class Snippet extends React.Component {
   render() {
     return (
-      <>
-        <img src={require(this.props.imagePath)} alt={this.props.imageAlt} height={180}></img> 
-        {this.props.details}
-      </>
+      <div className="snippet">
+        <div className="image">
+          <img src={this.props.imageSource} alt={this.props.imageAlt}></img>
+        </div>
+
+        <div className="detail">
+          <div className="vertical-groups">
+            {this.props.details}
+            <div className="detail-eye-candy horizontal-groups center-centric">
+              <div className="logos">
+                {
+                  this.props.logos ? 
+                    this.props.logos.map(function(logo) {
+                      return <img src={logo.source} alt={logo.alt} height={100}></img>
+                    })
+                    : <></>
+                }
+              </div>
+              <div className="buttons">
+                {
+                  this.props.buttons ?
+                    this.props.buttons.map(function(button) {
+                      return <a href={button.link} target="_blank" rel="noreferrer">
+                        <Button variant={button.variant}>{button.text}</Button>
+                      </a>
+                    })
+                    : <></>
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     ) 
   }
 }
--- my-website/src/pages/about.js
+++ my-website/src/pages/about.js
@@ -17,7 +17,7 @@ function About() {
                 <img src={require("../assets/logos/Python logo.png")} height="100" alt="Python"></img>
               </div>
               <hr></hr>
-              <div className="snippet">
+              <div className="text">
                 <p>
                   Skilled in conventional object oriented programing languages such as <b>C++</b>, <b>Java</b>, and <b>Python</b>. They are great for compiled programs and neat to know for 
                   more complex ideas. In addition, popular game engines like Unity or Unreal Engine 
@@ -33,7 +33,7 @@ function About() {
                 <img src={require("../assets/logos/Bootstrap logo.png")} height="100" alt="Bootstrap"></img>
               </div>
               <hr></hr>
-              <div className="snippet">
+              <div className="text">
                 <p>
                   Experienced with <b>JavaScript</b>, <b>React</b>, and <b>Bootstrap</b>. It's what I used to 
                   make this website! They are well documented and their coding syntaxes 
@@ -48,7 +48,7 @@ function About() {
                 <img src={require("../assets/logos/Node logo.png")} height="100" alt="Node"></img>
               </div>
               <hr></hr>
-              <div className="snippet">
+              <div className="text">
                 <p>
                   Familiar with database management systems (DBMS) such as <b>MySQL</b> and <b>PostgreSQL</b>.
                   I like to use <b>Node.js</b> to send requests and queries to these DBMSs.
@@ -66,26 +66,31 @@ function About() {
           <h2>HDL Research Labs <i>(09/2019 - 08/2020)</i></h2>
           <h3>Software Engineer Internship</h3>
 
-          <div className="horizontal-groups center-centric">
-
-            <Snippet
-              imagePath="../assets/logos/HDL Research Lab.png"
-              imageAlt="HDL Research Lab"
-              details={
-                <p>
-                  Interned at <a href="https://hdlresearchlab.com/" target="_blank" rel="noreferrer">HDL Research Lab</a>. 
-                  Responsible for designing and developing a server-client database program using Qt for the front-end and C++ 
-                  with Boost libraries for the back-end. The program helped manage the flow of manufactured 
-                  parts within the company, assisting and supporting over 100 employees. It was compatible with 
-                  Windows XP, Vista, 7, 8, 10, and Linux subsystems and featured a login system with user
-                  roles to help manage permissions within the program.
-                  <br></br><br></br>
-                  In addition, assisted in internal IT networking, and setted up a cross-OS file sharing system on an Ubuntu server with 
-                  file access permissions based on roles in the company.
-                </p>
-              }
-            />
-          </div>
+          <Snippet
+            imageSource={require("../assets/images/HDL Research Lab Building.png")}
+            imageAlt={"HDL Research Lab"}
+            details={
+              <p>
+                Interned at <a href="https://hdlresearchlab.com/" target="_blank" rel="noreferrer">HDL Research Lab</a>. 
+                Responsible for designing and developing a server-client database program using Qt for the front-end and C++ 
+                with Boost libraries for the back-end. The program helped manage the flow of manufactured 
+                parts within the company, assisting and supporting over 100 employees. It was compatible with 
+                Windows XP, Vista, 7, 8, 10, and Linux subsystems and featured a login system with user
+                roles to help manage permissions within the program.
+                <br></br><br></br>
+                In addition, assisted in internal IT networking, and setted up a cross-OS file sharing system on an Ubuntu server with 
+                file access permissions based on roles in the company.
+              </p>
+            }
+            logos={[
+              {source: require("../assets/logos/C++ logo.png"), alt: "C++"},
+              {source: require("../assets/logos/Qt logo.png"), alt: "Qt"},
+              {source: require("../assets/logos/Boost logo.png"), alt: "Boost"}
+            ]}
+            buttons={[
+              {link: "https://hdlresearchlab.com/", text: "HDL's website", variant: "primary"}
+            ]}
+          />
 
           <h1>Projects</h1>
           <br></br>
